a9082562ee00fc1e4300e96721142f3c83ed06b5
Hannu Suhonen <hannu.suhonen@taika3d.com>
2023-02-02 17:13:18 +0200

    Timer functionality

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,6 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBAction func eggButtonPress(_ sender: UIButton) {
-        print(sender.titleLabel!.text!)
+        
+        if(sender.titleLabel?.text == "Soft"){
+            print("Timer set to 2")
+            TimerSet(seconds: 2)
+        }
+        else if(sender.titleLabel?.text == "Medium"){
+            print("Timer set to 5")
+            TimerSet(seconds: 5)
+        }
+        else{
+            print("Timer set to 10")
+            TimerSet(seconds: 10)
+        }
+    }
+    
+    func TimerSet(seconds: Double){
+        Timer.scheduledTimer(timeInterval: seconds,target: self,selector: #selector(fireTimer),userInfo: nil,repeats: false)
+    }
+    @objc func fireTimer() {
+        print("Timer fired!")
     }
 }
