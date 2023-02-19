da0ddbbd844464052b9ab34b0f1298ce18fd4e5f
Hannu Suhonen <hannu.suhonen@taika3d.com>
2023-02-19 08:48:06 +0200

    progress bar added

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -8,28 +8,34 @@
 import UIKit
 
 var counter = 30
+var selectedTime = 1
 weak var gameTimer: Timer?
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var progressView: UIProgressView!
     @IBOutlet weak var timeText: UILabel!
+    
     
     @IBAction func eggButtonPress(_ sender: UIButton) {
         gameTimer?.invalidate()
         
         let title = sender.titleLabel?.text
-        let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+        let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 700]
         
         switch title {
         case "Soft":
             print("Timer set to 5 minutes!")
             counter = eggTimes["Soft"]!
+            selectedTime = counter
         case "Medium":
             print("Timer set to 7 minutes")
             counter = eggTimes["Medium"]!
+            selectedTime = counter
         case "Hard":
             print("Timer set to 12 minutes")
             counter = eggTimes["Hard"]!
+            selectedTime = counter
         default:
             print("testing!")
         }
@@ -43,6 +49,7 @@ class ViewController: UIViewController {
         //example functionality
         if counter > 0 {
             counter -= 1
+            progressView.progress = Float(counter)/Float(selectedTime)
             timeText.text = String(counter)
         }
         else if(counter == 0){
